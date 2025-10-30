927eced5d7da9f527f8605cf97d9e90a4475071b
Geoff Speirs <geoff.dotvital@gmail.com>
2025-10-31 07:04:30 +1100

    process json

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::io;
-
-fn main() -> io::Result<()> {
-    let stdin = io::read_to_string(io::stdin())?;
-    print!("{stdin}");
+fn main() -> Result<(), serde_json::Error> {
+    use serde_json;
+    use serde_json::Value;
+    use std::io;
+    let json: Value = serde_json::from_reader(io::stdin())?;
+    print!("{json}");
     Ok(())
 }
--- tests/main_test.rs
+++ tests/main_test.rs
@@ -1,5 +1,5 @@
 #[test]
-fn main_pipes_stdin_to_stdout() {
+fn errors_with_code_1_for_invalid_json() {
     use std::process::Command;
     use std::process::Stdio;
     let mut sarifup = Command::new("target/debug/sarifup")
@@ -11,9 +11,32 @@ fn main_pipes_stdin_to_stdout() {
     let stdin = sarifup.stdin.as_mut().unwrap();
 
     use std::io::Write;
-    stdin.write_all(b"42\n").unwrap();
+    stdin.write_all(b"d{").unwrap();
+
+    let output = sarifup.wait_with_output().expect("Failed to read output!");
+    assert!(!output.status.success());
+    assert_eq!(output.status.code().unwrap(), 1);
+}
+
+#[test]
+fn outputs_with_success_for_valid_json() {
+    use std::process::Command;
+    use std::process::Stdio;
+    let mut sarifup = Command::new("target/debug/sarifup")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to spawn app!");
+
+    let stdin = sarifup.stdin.as_mut().unwrap();
+
+    use std::io::Write;
+    stdin.write_all(b"{\"test\": \"test\"}").unwrap();
 
     let output = sarifup.wait_with_output().expect("Failed to read output!");
     assert!(output.status.success());
-    assert_eq!(String::from_utf8_lossy(&output.stdout), "42\n");
+    assert_eq!(
+        String::from_utf8_lossy(&output.stdout),
+        "{\"test\":\"test\"}"
+    );
 }
