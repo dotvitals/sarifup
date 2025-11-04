3ec72a7c09de20f88d107952c5a00610b145aaab
Geoff Speirs <geoff.dotvital@gmail.com>
2025-11-04 17:12:34 +1100

    error if missing filename arg

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@ fn main() -> Result<(), serde_json::Error> {
     use serde_sarif::sarif::Sarif;
     use std::io;
     let sarif: Sarif = serde_json::from_reader(io::stdin())?;
+
+    use std::env;
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Please provide previous sarif filename.");
+    }
+
     let json: String = serde_json::to_string(&sarif).unwrap();
     print!("{json}");
     Ok(())
--- tests/main_test.rs
+++ tests/main_test.rs
@@ -1,11 +1,16 @@
+use std::io::Write;
 use std::process::Child;
+use std::process::Command;
+use std::process::Stdio;
+
+const VALID_SARIF_STR: &[u8] =
+    b"{\"runs\":[{\"tool\":{\"driver\":{\"name\":\"test\"}}}],\"version\":\"1.0.0\"}";
 
 fn create_sarifup() -> Child {
-    use std::process::Command;
-    use std::process::Stdio;
     Command::new("target/debug/sarifup")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
+        .arg("test.json")
         .spawn()
         .expect("Failed to spawn app!")
 }
@@ -15,7 +20,6 @@ fn errors_with_invalid_json() {
     let mut sarifup = create_sarifup();
     let stdin = sarifup.stdin.as_mut().unwrap();
 
-    use std::io::Write;
     stdin.write_all(b"d{").unwrap();
 
     let output = sarifup.wait_with_output().expect("Failed to read output!");
@@ -29,7 +33,6 @@ fn errors_for_invalid_sarif() {
 
     let missing_ver_sarif_str = b"{\"runs\":[{\"tool\":{\"driver\":{\"name\":\"test\"}}}]}";
 
-    use std::io::Write;
     stdin.write_all(missing_ver_sarif_str).unwrap();
 
     let output = sarifup.wait_with_output().expect("Failed to read output!");
@@ -41,27 +44,35 @@ fn succeeds_for_valid_sarif() {
     let mut sarifup = create_sarifup();
     let stdin = sarifup.stdin.as_mut().unwrap();
 
-    let sarif_str =
-        b"{\"runs\":[{\"tool\":{\"driver\":{\"name\":\"test\"}}}],\"version\":\"1.0.0\"}";
-
-    use std::io::Write;
-    stdin.write_all(sarif_str).unwrap();
+    stdin.write_all(VALID_SARIF_STR).unwrap();
 
     let output = sarifup.wait_with_output().expect("Failed to read output!");
     assert!(output.status.success());
 }
 
 #[test]
-fn returns_input_for_valid_sarif() {
+fn returns_input_for_valid_sarif_input() {
     let mut sarifup = create_sarifup();
     let stdin = sarifup.stdin.as_mut().unwrap();
 
-    let sarif_str =
-        b"{\"runs\":[{\"tool\":{\"driver\":{\"name\":\"test\"}}}],\"version\":\"1.0.0\"}";
-
-    use std::io::Write;
-    stdin.write_all(sarif_str).unwrap();
+    stdin.write_all(VALID_SARIF_STR).unwrap();
 
     let output = sarifup.wait_with_output().expect("Failed to read output!");
-    assert_eq!(&output.stdout, sarif_str);
+    assert_eq!(&output.stdout, VALID_SARIF_STR);
+}
+
+#[test]
+fn erorrs_when_missing_filename_arg() {
+    let mut sarifup = Command::new("target/debug/sarifup")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to spawn app!");
+
+    let stdin = sarifup.stdin.as_mut().unwrap();
+
+    stdin.write_all(VALID_SARIF_STR).unwrap();
+
+    let output = sarifup.wait_with_output().expect("Failed to read output!");
+    assert!(!output.status.success());
 }
